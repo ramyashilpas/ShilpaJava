91105190b4dfbfc147cd28d716a59cd6cbe745b5
ramyashilpas <ramyashilpas@gmail.com>
2021-05-19 19:11:28 +0100

    hackerRank-solved files changes

--- ShilpaJava/src/com/shilpa/StringTopic/StringEasyProblem3.java
+++ ShilpaJava/src/com/shilpa/StringTopic/StringEasyProblem3.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class StringEasyProblem3 {
 
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest;
-        String largest;
+        String smallest="";
+        String largest="";
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
